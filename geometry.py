6cc2bf6cfb34a8d3c57284f87a6394bb11c8dc05
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-02-17 15:22:59 +0100

    added the other geometry vars to Geometry

--- geometry.py
+++ geometry.py
@@ -1,20 +1,34 @@
 import numpy as np
+from data.consts import parameters_geometry
 
 class Geometry:
     def __init__(self, **kwargs):
         self.c_a = kwargs.get("c_a")
         self.l_a = kwargs.get("l_a")
 
-        # self.x_1 = kwargs["x_1"]
-        # self.x_2 = kwargs["x_2"]
-        # self.x_3 = kwargs["x_3"]
+        self.x_1 = kwargs.get("x_1")
+        self.x_2 = kwargs.get("x_2")
+        self.x_3 = kwargs.get("x_3")
 
+        self.x_a = kwargs.get("x_a")
+
+        self.h = kwargs.get("h")
+        self.t_sk = kwargs.get("t_sk")
+        self.t_sp = kwargs.get("t_sp")
+
+        self.t_st = kwargs.get("t_st")
+        self.h_st = kwargs.get("h_st")
+        self.w_st = kwargs.get("w_st")
+        self.n_st = kwargs.get("n_st")
         #TODO: add the rest of the geometry inputs
         pass
 
     @property
     def MMoI(self):
-        return f"Here is the length of the aileron : {self.l_a}"
+        print(self.c_a) # we can access things now
+        print(self.x_a)
+
+        return self.c_a + self.x_a
 
     @property
     def centroid(self):
@@ -26,9 +40,7 @@ class Geometry:
 
 
 if __name__ == "__main__": # is called when you run the script
-    params = {"l_a" : 1.2,
-              "c_a" : 0.4}
-
-    geo = Geometry(**params)
+    # call an instance of the class
+    geo = Geometry(**parameters_geometry) 
 
     print(geo.MMoI)
