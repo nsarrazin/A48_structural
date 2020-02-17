a79d35e4771a01b00bdc1d9d85d97bb0a43dbf65
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-02-17 15:12:07 +0100

    basic OOP demo

--- geometry.py
+++ geometry.py
@@ -2,12 +2,19 @@ import numpy as np
 
 class Geometry:
     def __init__(self, **kwargs):
-        d1 = kwargs["d1"] # do the same for other vars
+        self.c_a = kwargs.get("c_a")
+        self.l_a = kwargs.get("l_a")
+
+        # self.x_1 = kwargs["x_1"]
+        # self.x_2 = kwargs["x_2"]
+        # self.x_3 = kwargs["x_3"]
+
+        #TODO: add the rest of the geometry inputs
         pass
 
     @property
     def MMoI(self):
-        return
+        return f"Here is the length of the aileron : {self.l_a}"
 
     @property
     def centroid(self):
@@ -16,3 +23,12 @@ class Geometry:
     @property
     def shearcenter(self):
         return
+
+
+if __name__ == "__main__": # is called when you run the script
+    params = {"l_a" : 1.2,
+              "c_a" : 0.4}
+
+    geo = Geometry(**params)
+
+    print(geo.MMoI)
