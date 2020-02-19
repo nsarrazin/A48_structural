2cf903b7ff2a6b1eef866f3f70e324c1c5bf7e6f
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-02-19 12:13:38 +0100

    added interp object to simulation class

--- simulation.py
+++ simulation.py
@@ -1,12 +1,13 @@
 import numpy as np
 from geometry import Geometry
 from matrix import LoadCase
-
+from interpolation import Interpolation
 
 class Simulation:
     def __init__(self, **kwargs):
         self.geo = Geometry(**kwargs)
         self.case = LoadCase(parent, **kwargs)
+        self.interp = Interpolation()
         
     def run():
         pass
