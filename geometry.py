c8f7fbb180096e79df24db5bb9b8cdb22184d439
pepgijsberts <61149267+pepgijsberts@users.noreply.github.com>
2020-02-20 14:10:50 +0100

    update 14:10

--- geometry.py
+++ geometry.py
@@ -213,9 +213,11 @@ class Geometry:
         l = self.c_a - self.h/2
         alpha = np.arctan2(self.h/2,l)
         d = l/np.cos(alpha)
-        d_z =(self.t_sk*self.h*self.h*d*d*(1/3-np.cos(alpha)/l-(self.h*np.cos(alpha))/(3*l))+self.h*self.h*((self.t_sk*self.h*self.h)/4-2*self.t_sk*self.h*self.h+(4*d*np.cos(alpha))/(self.h*self.h)+(self.t_sk*self.h*self.h)/4))/(4*4.753851442684436e-06)
+        d_z =(self.t_sk*self.h*self.h*d*d*(1./3.-np.cos(alpha)/l-(self.h*np.cos(alpha))/(3*l))+self.h*self.h*((self.t_sk*self.h*self.h)/4-2*self.t_sk*self.h*self.h+(4*d*np.cos(alpha))/(self.h*self.h)+(self.t_sk*self.h*self.h)/4))/(4*4.753851442684436e-06)
         
         return d_z
+
+        #'Qz': -0.0004095185617891304, 'Qy': 0.0
 
 
 if __name__ == "__main__": # is called when you run the script
