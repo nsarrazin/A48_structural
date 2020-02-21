db87c7759422345f78f6222c51a58dbb6d8624a8
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-02-21 11:43:38 +0100

    removed the zeros

--- solution.py
+++ solution.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from helpers import step
 
-
 class Solution:
     def __init__(self, parent):
         self.parent = parent
@@ -26,7 +25,7 @@ class Solution:
         return -1/(self.case.E*self.geo.MMoI[0])*(sol["Fa"]*self.case.a_z/6*step(x,self.geo.x_2-self.geo.x_a/2)**3\
             +self.case.P*self.case.a_z*step(x,self.geo.x_2+self.geo.x_a/2)**3\
                 -sol["Fz_1"]/6*step(x,self.geo.x_1)**3-sol["Fz_2"]/6*step(x,self.geo.x_2)**3-sol["Fz_3"]/6*step(x,self.geo.x_3)**3\
-                    +sol["C3"]*x+sol["C4"]*0)       
+                    +sol["C3"]*x+sol["C4"])       
 
 
     def v_z(self, x):
@@ -34,7 +33,7 @@ class Solution:
         return -1/(self.case.E*self.geo.MMoI[0])*(sol["Fa"]*self.case.a_z/6*step(x,self.geo.x_2-self.geo.x_a/2)**3\
             +self.case.P*self.case.a_z*step(x,self.geo.x_2+self.geo.x_a/2)**3\
                 -sol["Fz_1"]/6*step(x,self.geo.x_1)**3-sol["Fz_2"]/6*step(x,self.geo.x_2)**3-sol["Fz_3"]/6*step(x,self.geo.x_3)**3\
-                    +sol["C3"]*x+sol["C4"]*0)
+                    +sol["C3"]*x+sol["C4"])
 
     def theta(self, x):
         sol = self.sol
@@ -43,6 +42,7 @@ class Solution:
                     +sol["Fa"]*self.case.a_y*self.case.z_sc*step(x,self.geo.x_2-self.geo.x_a/2)**1-sol["Fa"]*self.case.a_m*step(x,self.geo.x_2-self.geo.x_a/2)**1\
                         +sol["C5"]\
                         +self.parent.interp.integrate_tau(x, z_sc=self.geo.h/2+self.case.z_sc, ord=2)[-1] + self.case.P*self.case.a_y*self.case.z_sc*step(x,self.geo.x_2+self.geo.x_a/2)**1-self.case.P*self.case.a_m*step(x,self.geo.x_2+self.geo.x_a/2)**1)
+    
     def v_y_prime(self,x):
         return self.v_y(x)*np.cos(-self.case.defl)+self.v_z(x)*np.sin(-self.case.defl)
     
@@ -52,9 +52,11 @@ class Solution:
     def plot(self):
 
         xs = np.linspace(0, self.geo.l_a, 100)
-        ys = [self.theta(i) for i in xs]
+        ys = [self.v_y_prime(i)+self.theta(i)*self.case.z_sc for i in xs]
 
-        offset = self.v_y(self.geo.x_2)*0
+        print(self.v_y_prime(self.geo.x_1)+self.theta(self.geo.x_1)*self.case.z_sc)
+
+        offset = self.v_y_prime(self.geo.x_1)+self.theta(self.geo.x_1)*self.case.z_sc
 
         plt.plot(xs, ys-offset)
         
@@ -65,5 +67,3 @@ class Solution:
             plt.axhline(y=y, linestyle="dashed", linewidth=1.2, color=f"C{n}")
 
         plt.show()
-
-            
