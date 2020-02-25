7f561f82ca12a3f22bc7e9a8f9d67debcd82d045
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2020-02-25 16:53:15 +0100

    updated step function

--- helpers.py
+++ helpers.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 
-def step(x, x_ref):
-    return np.clip(x-x_ref, 0, np.inf)
+def step(x, x_ref, pow=1):
+    val = np.clip(x-x_ref, 0, np.inf)
+    if val == 0 and pow == 0:
+        return 0
+    return val**pow
